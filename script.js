884604e6584529300aa3d537ebc7fffa8d8d78a4
SUDHANSHU <pradhansudhanshu7037@gmail.com>
2026-03-10 13:42:14 +0530

    all files

---
+++ script.js
@@ -0,0 +1,22 @@
+// console.log("Hello world");
+// // document.write("hello world");
+// var num =6;
+// console.log(num);
+// console.log(typeof(num));
+// var person={
+//     name:'sudhanshu',
+//     age:'51',
+//     tittle:'jiedbibcewi',
+// }
+// console.log(person);
+const sym1=Symbol(4)
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2)
+{
+    console.log("true");
+}
+else{
+    console.log("false");
+}
